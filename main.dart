98bf496fda452511bd68da4fe489263b61147e98
KunalKhadgi <132919518+KunalKhadgi@users.noreply.github.com>
2023-07-12 13:15:55 +0530

    Create main.dart
    Added main.dart

---
+++ main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(CounterApp());
+}
+
+class CounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CounterPage(),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter -= 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Counter Value:',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              '$_counter',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+
+            ElevatedButton(
+                child: const Text('Minus'),
+              style: const ButtonStyle(alignment: Alignment.centerLeft  ),
+              onPressed: _decrementCounter,
+            ),
+
+            ElevatedButton(
+              child: const Text('Plus'),
+              style: const ButtonStyle(alignment: Alignment.centerRight  ),
+              onPressed: _incrementCounter,
+            ),
+
+          ],
+        ),
+      ),
+
+
+
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ),
+    );
+  }
+}
