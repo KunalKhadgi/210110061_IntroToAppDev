896cca4ed91f6cb92ddeb647075e1fd646c61f49
Kunal Khadgi <132919518+KunalKhadgi@users.noreply.github.com>
2023-07-20 22:47:43 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 void main() {
   runApp(CounterApp());
@@ -7,9 +9,9 @@ class CounterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Counter App',
+      title: 'Budget Tracker',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       home: CounterPage(),
     );
@@ -23,11 +25,27 @@ class CounterPage extends StatefulWidget {
 
 class _CounterPageState extends State<CounterPage> {
   int _counter = 0;
+  var _value1 = TextEditingController();
+  var _value2 = TextEditingController();
+  var _value3 = TextEditingController();
+  var _value4 = TextEditingController();
 
-  void _incrementCounter() {
+
+  void _update(){
+
+    dynamic _temp1 = _value1;
+   // print(_temp1.runtimeType);
+  //  _counter +=
+
+  }
+
+  void _incrementCounter(int value) {
     setState(() {
-      _counter++;
+      _counter += value;
+      // return null;
     });
+    // return _counter;
+
   }
 
   void _decrementCounter() {
@@ -36,40 +54,237 @@ class _CounterPageState extends State<CounterPage> {
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Counter App'),
+        title: Center(child: Text('Budget Tracker',)),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Counter Value:',
-              style: TextStyle(fontSize: 20),
-            ),
-            Text(
-              '$_counter',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-            ),
+      body:
 
-            ElevatedButton(
-                child: const Text('Minus'),
-              style: const ButtonStyle(alignment: Alignment.centerLeft  ),
-              onPressed: _decrementCounter,
-            ),
+    Padding(
+        padding: const EdgeInsets.fromLTRB(20, 30, 20, 0),
+        child: Center(
 
-            ElevatedButton(
-              child: const Text('Plus'),
-              style: const ButtonStyle(alignment: Alignment.centerRight  ),
-              onPressed: _incrementCounter,
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
 
-          ],
+
+              //  Icon
+
+
+              const Padding(
+                padding: EdgeInsets.all(30),
+                child: Icon(
+                  Icons.person,
+                  size: 90.0,
+                ),
+              ),
+
+
+              //  Total: $_counter
+
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 10, 30, 40),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Total: \$',
+                        style: TextStyle(fontSize: 30, letterSpacing: 2, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '$_counter',
+                        style: TextStyle(fontSize: 30, letterSpacing: 2, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+
+
+             //  Expense  1
+
+
+             Column (
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        width: 157,
+                        child: TextField(
+                          keyboardType: TextInputType.text,
+                          decoration:  InputDecoration(
+                            hintText: 'Reason',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+
+                    SizedBox(width: 20.0),
+
+                    Container(
+                      width: 85,
+                      child: TextField(
+                          keyboardType: TextInputType.number,
+                          controller: _value1,
+                          decoration:  InputDecoration(
+                            hintText: 'Amount',
+                            //textAlign: ,
+                            border: OutlineInputBorder(),
+                      )
+                    )
+                    ),
+                    Container(width: 10,),
+                    IconButton(onPressed: _update, icon: Icon(Icons.adb_outlined),),
+                  //  IconButton(onPressed: {_counter += _value}, icon: )
+                ],),
+              ],
+            ),                    SizedBox(height: 20.0),
+
+
+              //  Expense 2
+
+
+              Column (
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        width: 157,
+                        child: TextField(
+                          keyboardType: TextInputType.text,
+                          decoration:  InputDecoration(
+                            hintText: 'Reason',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+
+                    SizedBox(width: 20.0),
+
+                    Container(
+                      width: 85,
+                      child: TextField(
+                          keyboardType: TextInputType.number,
+                          controller: _value2,
+                          decoration:  InputDecoration(
+                            hintText: 'Amount',
+                            //textAlign: ,
+                            border: OutlineInputBorder(),
+                      )
+                    )
+                    ),
+                    Container(width: 10,),
+                    IconButton(onPressed: _update, icon: Icon(Icons.adb_outlined),),
+                  //  IconButton(onPressed: {_counter += _value}, icon: )
+                ],),
+              ],
+            ),                    SizedBox(height: 20.0),
+
+
+              //   Expense 3
+
+
+              Column (
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        width: 157,
+                        child: TextField(
+                          keyboardType: TextInputType.text,
+                          decoration:  InputDecoration(
+                            hintText: 'Reason',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+
+                    SizedBox(width: 20.0),
+
+                    Container(
+                      width: 85,
+                      child: TextField(
+                          keyboardType: TextInputType.number,
+                          controller: _value3,
+                          decoration:  InputDecoration(
+                            hintText: 'Amount',
+                            //textAlign: ,
+                            border: OutlineInputBorder(),
+                      )
+                    )
+                    ),
+                    Container(width: 10,),
+                    IconButton(onPressed: _update, icon: Icon(Icons.adb_outlined),),
+                  //  IconButton(onPressed: {_counter += _value}, icon: )
+                ],),
+              ],
+            ),                    SizedBox(height: 20.0),
+
+
+              //  Expense 4
+
+
+              Column (
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        width: 157,
+                        child: TextField(
+                          keyboardType: TextInputType.text,
+                          decoration:  InputDecoration(
+                            hintText: 'Reason',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+
+                    SizedBox(width: 20.0),
+
+                    Container(
+                      width: 85,
+                      child: TextField(
+                          keyboardType: TextInputType.number,
+                          controller: _value4,
+                          decoration:  InputDecoration(
+                            hintText: 'Amount',
+                            //textAlign: ,
+                            border: OutlineInputBorder(),
+                      )
+                    )
+                    ),
+                    Container(width: 10,),
+                    IconButton(onPressed: _update, icon: Icon(Icons.adb_outlined),),
+                  //  IconButton(onPressed: {_counter += _value}, icon: )
+                ],),
+              ],
+            )
+
+            ],
+          ),
         ),
       ),
+
+
 
 
 
